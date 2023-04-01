b9b2fbb6c3f057aee746ceb12bc92791e173a479
Prasad Sawool <thc.prasads@gmail.com>
2023-04-01 18:21:35 +0530

    added git plugin and basic git remap

---
+++ after/plugin/fugitive.lua
@@ -0,0 +1,2 @@
+vim.keymap.set("n", "<leader>gg", vim.cmd.G)
+vim.keymap.set("n", "<leader>gl", vim.cmd.Gclog)
--- lua/thc/remap.lua
+++ lua/thc/remap.lua
@@ -1,11 +1,7 @@
 local vim = vim
 local global = vim.g
 
-
 global.mapleader = " "
-
 
 vim.keymap.set("n", "<leader>nn", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>bk", vim.cmd.bd)
-vim.keymap.set("n", "<leader>gg", vim.cmd.G)
-vim.keymap.set("n", "<leader>gl", vim.cmd.Gclog)
