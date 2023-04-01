874506940f4b4c2016faa71e2a81468324d49047
Prasad Sawool <thc.prasads@gmail.com>
2023-04-01 18:12:28 +0530

    added new remaps

--- lua/thc/remap.lua
+++ lua/thc/remap.lua
@@ -8,3 +8,4 @@ global.mapleader = " "
 vim.keymap.set("n", "<leader>nn", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>bk", vim.cmd.bd)
 vim.keymap.set("n", "<leader>gg", vim.cmd.G)
+vim.keymap.set("n", "<leader>gl", vim.cmd.Gclog)
