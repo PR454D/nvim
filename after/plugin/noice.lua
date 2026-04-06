dafb4960e222f7b59e5453b8e40787216e6f27f2
Prasad Sawool <77245742+PR454D@users.noreply.github.com>
2026-04-06 13:56:39 +0530

    add noice config for lsp

--- after/plugin/noice.lua
+++ after/plugin/noice.lua
@@ -45,6 +45,10 @@ noice.setup({
     override = {
       ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
       ["vim.lsp.util.stylize_markdown"] = true,
+      ["cmp.entry.get_documentation"] = true,
     },
+    messages = { enabled = false },
+    hover = { enabled = false },
+    signature = { enabled = true },
   },
 })
